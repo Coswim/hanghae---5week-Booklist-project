ac90d7739313250efdd9c0eee76341b30703c885
Forza5 <ganp433@gmail.com>
2022-10-17 09:11:36 +0900

    수정

--- week5/src/pages/Main.jsx
+++ week5/src/pages/Main.jsx
@@ -4,9 +4,7 @@ import { useNavigate } from 'react-router-dom';
 function Main() {
   const navigate = useNavigate();
 
-  return (
-    <div />
-  );
+  return <div></div>;
 }
 
 export default Main;
